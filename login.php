36eca38ee3645ac97ca8ff0898eb19e275404acb
Dieter Dewachter <dieter.dewachter@outlook.be>
2020-04-21 13:00:17 +0200

    Pulled laravel files

--- login.php
+++ login.php
@@ -19,7 +19,7 @@
         if (isset($_POST["password"])) // login
         {
             $query = $db->prepare("SELECT password FROM accounts WHERE username = ?");
-            $query->bind_param("s", $_POST['username']);
+            $query->bind_param("s", $_POST['username1']);
             $result = $query->execute();
 
             if ($result->num_rows > 0)
@@ -65,63 +65,6 @@
         $db->close();
     }
 ?>
-<style>
-html {
-    font-family: sans-serif;
-    background-color: #fefefe;
-}
-body {
-    height: 100%;
-    position: fixed;
-    width: 100%;
-}
-#logincontainer {
-    height: 100%;
-    width: 100%;
-    position: fixed;
-    top: 0;
-    left: 0;
-    background-color: rgba(0, 0, 0, 0.2);
-    display: none;
-}
-.popup {
-    min-height: 800px;
-    width: 80%;
-    max-width: 700px;
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-}
-.popup .title {
-    background-color: rgb(34, 34, 34);
-    border-top-left-radius: 5px;
-    border-top-right-radius: 5px;
-    color: white;
-    padding: 16px 24px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-}
-
-.popup .actions {
-    background-color: #eeeeee;
-    border-bottom-left-radius: 5px;
-    border-bottom-right-radius: 5px;
-    display: flex;
-    justify-content: flex-end;
-    align-content: center;
-    padding: 16px 24px;
-}
-
-button{
-    margin: 10px 10px;
-}
-
-.close {
-    cursor: pointer;
-}
-</style>
 <!doctype html>
 <html lang="en">
   <head>
@@ -132,7 +75,9 @@ button{
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <link href="css/styles.css" type="text/css" rel="stylesheet" />
+    <script src="js/app.js"></script>
+    <title>Login testing</title>
   </head>
   <body>
     <?php if ($status) { ?>
@@ -140,63 +85,60 @@ button{
             <?php echo $status; ?>
         </div>
     <?php } ?>
-    <div id="login">
-        <form method="POST" action="login.php">
-            <div class="form-group">
-                <label for="username1">Username</label>
-                <input type="text" name="username" id="username1" placeholder="Your name" />
-            </div>
-            <div class="form-group">
-                <label for="password3">Password</label>
-                <input type="password" name="password" id="password3" placeholder="Password" />
-            </div>
-            
-            <input type="submit" class="btn btn-outline-success" value="Login" />
-        </form>
-    </div>
-
-    
-    <button onclick="showPopup()" class="btn btn-outline-success">Register</button>
+    <button onclick="showPopupLogin()" class="btn btn-outline-success">Login</button>
     <form id="logincontainer" method="POST" action="login.php">
         <div class="popup">
             <div class="title">
+                Login
+                <input type="button" class="btn btn-danger" value="X" onclick="cancelLogin()"/>
+            </div>
+            <div id="login">
+                <div class="form-group">
+                    <label for="username1">Username</label>
+                    <input type="text" name="username" id="username1" placeholder="Your name" minlength="3" required="true" />
+                </div>
+                <div class="form-group">
+                    <label for="password3">Password</label>
+                    <input type="password" name="password" id="password3" placeholder="Password" minlength="6" required="true" pattern="^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$" />
+                </div>
+            </div>
+            <div class="actions">
+                <input type="submit" class="btn btn-outline-success" value="Login" />
+                <input type="button" class="btn btn-outline-danger" value="Cancel" onclick="cancelLogin()" />
+            </div>
+        </div>
+    </form>
+
+    
+    <button onclick="showPopupRegister()" class="btn btn-outline-success">Register</button>
+    <form id="registercontainer" method="POST" action="login.php">
+        <div class="popup">
+            <div class="title">
                 Register
-                <button class="btn btn-primary" onclick="cancel()">X</button>
+                <input type="button" class="btn btn-danger" value="X" onclick="cancelRegister()"/>
             </div>
             <div id="register">
                 <div class="form-group">
-                    <label for="username1">Username</label>
-                    <input type="text" name="username" id="username1" placeholder="Your name" />
+                    <label for="username2">Username</label>
+                    <input type="text" name="username" id="username2" placeholder="Your name" minlength="3" required="true" />
                 </div>
                 <div class="form-group">
                     <label for="password4">Password</label>
-                    <input type="password" name="password1" id="password4" placeholder="Password" />
+                    <input type="password" name="password1" id="password4" placeholder="Password" minlength="6" required="true" pattern="^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$" />
                 </div>
                 <div class="form-group">
                     <label for="password5">Repeat</label>
-                    <input type="password" name="password2" id="password5" placeholder="Password" />
+                    <input type="password" name="password2" id="password5" placeholder="Password" minlength="6" required="true" pattern="^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$" />
                 </div>
             </div>
             <div class="actions">
                 <input type="submit" class="btn btn-outline-success" value="Register" />
-                <button class="btn btn-outline-success" onclick="cancel()">cancel</button>
+                <input type="button" class="btn btn-outline-danger" value="Cancel" onclick="cancelRegister()" />
             </div>
         </div>
     </form>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script>
-    function showPopup() {
-        document.getElementById('logincontainer').style.display = 'block'
-    }
-    function cancel() {
-        document.getElementById('logincontainer').style.display = 'none'
-    }
-    function change() {
-        document.getElementsByTagName('body')[0].style.backgroundColor = 'rgb(56, 130, 119)'
-        cancel()
-    }
-    </script>
   </body>
 </html>

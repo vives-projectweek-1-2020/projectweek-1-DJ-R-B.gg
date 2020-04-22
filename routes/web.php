3970e3bc48a60c924e563c504cbef611b2e7a956
rvis15 <reinder.vis@student.vives.be>
2020-04-22 16:14:58 +0200

    Created bootstraptable with data from table

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-<<<<<<< Updated upstream
-=======
 
 Route::get('/login', function () {
     return view('login');
@@ -29,4 +27,3 @@ Route::post('/login', function () {
 });
 Route::get('/issues','IssueController@index');
 
->>>>>>> Stashed changes

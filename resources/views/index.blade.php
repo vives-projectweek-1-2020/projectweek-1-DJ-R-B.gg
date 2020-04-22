92777c8c31f73eb85b3b643d7215012987e329ea
Dieter Dewachter <dieter.dewachter@outlook.be>
2020-04-22 09:39:13 +0200

    Added login to main php file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,67 @@
+<?php
+    // message for the user
+    $status = false;
+    $type = "alert-danger";
+
+    if (isset($_POST["username"]))
+    {
+        // database connection
+        //$db = new mysqli("127.0.0.1", "root", "", "pieter");
+        $username = htmlspecialchars($_POST['username']);
+
+        if (isset($_POST["password"])) // login
+        {
+            //$query = $db->prepare("SELECT password FROM accounts WHERE username = ?");
+            //$query->bind_param("s", $_POST['username']);
+            //$result = $query->execute();
+            
+            $result = DB::connection('mysql')->select("SELECT password FROM accounts WHERE username = ?", [ $_POST["username"] ]);
+
+            if (count($result) > 0)
+            {
+                $row = $result[0];
+            
+                if (password_verify($_POST["password"], $row->password))
+                {
+                    $_SESSION["username"] = $username;
+                    $status = "Successfully logged in as '$username'!";
+                    $type = "alert-success";
+                }
+                else
+                {
+                    $status = "Invalid password!";
+                }
+            }
+            else
+            {
+                $status = "Username not found";
+            }
+        }
+        else if (isset($_POST["password1"]) && isset($_POST["password2"])) // register
+        {
+            if ($_POST["password1"] != $_POST["password2"])
+            {
+                $status = "Passwords don't match!";
+            }
+            else
+            {
+                $password = password_hash($_POST["password1"], PASSWORD_DEFAULT);
+                $rank = 0;
+
+                //$query = $db->prepare("INSERT INTO accounts (username, password, rank) VALUES (?, ?, ?)");
+                //$query->bind_param("ssi", $_POST['username'], $password, $rank);
+                //$query->execute();
+
+                DB::connection('mysql')->insert("INSERT INTO accounts (username, password, rank) VALUES (?, ?, ?)", [ $_POST["username"], $password, $rank ]);
+
+                $status = "Successfully registered '$username'";
+                $type = "alert-success";
+            }
+        }
+
+        // $db->close();
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,15 +70,79 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Uploadzone</title>
     <meta name= "author" content="Brent Schaepdrijver">
+    <script src="{{ asset('js/app.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
   
+
+    <!-- Website layout -->
+
 	<div class="welcome">
         <header>Welkom, upload je taken hier.</header>
-        <button type="button" class="login" onclick="select('login')">Login hier</button> 
-        <button type="button" class="register" onclick="select('register')">Registreer hier</button>
+        <!-- LOGIN GEDEELTE -->
+
+    <?php if ($status) { ?>
+        <div class="alert <?= $type ?>" id="status">
+            <?php echo $status; ?>
+        </div>
+    <?php } ?>
+    <button onclick="showPopupLogin()" class="btn btn-outline-success">Login</button>
+    <form id="logincontainer" method="POST" action="/login">
+        @csrf
+        <div class="popup">
+            <div class="title">
+                Login
+                <input type="button" class="btn btn-danger" value="X" onclick="cancelLogin()"/>
+            </div>
+            <div id="login">
+                <div class="form-group">
+                    <label for="username1">Username</label>
+                    <input type="text" name="username" id="username1" placeholder="Your name" minlength="3" required="true" />
+                </div>
+                <div class="form-group">
+                    <label for="password3">Password</label>
+                    <input type="password" name="password" id="password3" placeholder="Password" minlength="6" required="true" pattern="^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$" />
+                </div>
+            </div>
+            <div class="actions">
+                <input type="submit" class="btn btn-outline-success" value="Login" />
+                <input type="button" class="btn btn-outline-danger" value="Cancel" onclick="cancelLogin()" />
+            </div>
+        </div>
+    </form>
+
+    
+    <button onclick="showPopupRegister()" class="btn btn-outline-success">Register</button>
+    <form id="registercontainer" method="POST" action="/login">
+        @csrf
+        <div class="popup">
+            <div class="title">
+                Register
+                <input type="button" class="btn btn-danger" value="X" onclick="cancelRegister()"/>
+            </div>
+            <div id="register">
+                <div class="form-group">
+                    <label for="username2">Username</label>
+                    <input type="text" name="username" id="username2" placeholder="Your name" minlength="3" required="true" />
+                </div>
+                <div class="form-group">
+                    <label for="password4">Password</label>
+                    <input type="password" name="password1" id="password4" placeholder="Password" minlength="6" required="true" pattern="^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$" />
+                </div>
+                <div class="form-group">
+                    <label for="password5">Repeat</label>
+                    <input type="password" name="password2" id="password5" placeholder="Password" minlength="6" required="true" pattern="^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$" />
+                </div>
+            </div>
+            <div class="actions">
+                <input type="submit" class="btn btn-outline-success" value="Register" />
+                <input type="button" class="btn btn-outline-danger" value="Cancel" onclick="cancelRegister()" />
+            </div>
+        </div>
+    </form>
              
     </div>
     <div class="choices">
@@ -24,8 +152,11 @@
     </div>
     
     <div class="upload">
-      <button  type="button" class="files" onclick="select('files')">Plaats je files hier om ze te uploaden.</button>
+      <input type="button" class="btn btn-outline-success" onclick="select('files')" value="Selecteer files om te uploaden"></button>
     </div>
+
+    
+
   
 </body>
 </html>

2caa49ae459cb36255aadcef9da2aa886147d84d
Brent Schaepdrijver <r0785603@student.vives.be>
2020-04-23 11:02:09 +0200

    textbox is placed

--- resources/views/wiskunde.blade.php
+++ resources/views/wiskunde.blade.php
@@ -8,42 +8,33 @@
     <meta name= "author" content="Brent Schaepdrijver">
     <script src="{{ asset('js/app.js') }}"></script>
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
-
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <link rel="stylesheet" href="{{ asset('css/wiskunde.css') }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>     
 @include('header')
-            <div class="upload">
-            <input type="button" class="btn btn-primary" class="sticky-top" value="Upload hier je wiskunde taak." />      
-        </div>
+            
 
     <div class="container">
         <div class="card text-center">
             <div class="card-header">Wiskunde</div>
                 <div class="card-body">  
-                <p class="card-text">De verbetering</p>
-                <h1 class="text-center"> Hier staat de oplossing van de taak </h1>
-                <div class="container pb-cmnt-container">
-    <div class="row">
-        <div class="col-md-6 col-md-offset-3"class="card text-center">
-            <div class="panel panel-info">
-                <div class="panel-body">
-                   
-                    <textarea placeholder="Write your comment here!" class="pb-cmnt-textarea" class="text-center"></textarea>
-                    <div class="float-left">
-                    <button class="btn btn-primary pull-right" type="button" >Share</button>
-</div>  
+                        <p class="card-text">De verbetering</p>
+                            <h1 class="text-center"> Hier staat de oplossing van de taak </h1>
+                                <div class="container pb-cmnt-container">
+                                <div class="form-group">
+                                 
+                                     <textarea class="form-control" rows="5" id="comment">Plaats je commentaar hier</textarea>
+                                     <button class="btn btn-primary pull-right" type="button" >Share</button>
+                                </div>
+                                <img src="..." class="img-fluid" alt="Responsive image">
                 </div>
-            </div>
+        </div>  
+                    
         </div>
-    </div>
-</div>
-                <img src="..." class="img-fluid" alt="Responsive image">
-                </div>
-   
+    </div> 
+</div>                       
 </div>
 </body>
 </html>

712053a442ba72391329078ac9af60398394eeff
Dieter Dewachter <dieter.dewachter@outlook.be>
2020-04-23 11:03:55 +0200

    Added migrations for table accounts

---
+++ database/migrations/2014_10_12_000000_create_accounts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Comments', function (Blueprint $table) {
+            $table->id()->unique();
+            $table->text('username');
+            $table->text('password');
+            $table->integer('rank');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Comments');
+    }
+}
